ab8a3944b5d98f00339fc39ca5dae44f20463b30
minecrawler <me@marco-alka.de>
2015-10-06 14:52:10 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Node-MB-String-Size
+
+Calculate the length of a string
+
+The problem when using string.length or even having a buffer to calculate its length is that certain character lengths (especially asian symbols) will be counted in a wrong way for UTF8 (namely 1 Byte / character).
+This might be a problem (e.g. for SHPS when sending the content-length). Node-MB-String-Size aims to solve it by adding to the byte count when special characters are detected.
+
+This module was first created for SHPS, but then separated for easy use by everyone :)
+
+### Version
+1.0.0
+
+### Installation
+```sh
+$ npm install node-mb-string-size
+```
+
+How To Use
+----
+
+```js
+var strLength = require('node-mb-string-size');
+
+var str = 'TEST - д÷ья???;';
+console.log('The string `' + str + '` is ' + strLength(str) + 'bytes in size.');
+```
+
+License
+----
+
+MIT
+
+**Free Software, Hell Yeah!**
---
+++ index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+
+/**
+ * Calculate length of a string
+ * The problem when using string length or even buffer length is that certain character lengths (especially asian symbols) will be counted in a wrong way for UTF8
+ * This might be a problem (e.g. for SHPS when sending the content-length)
+ * This function aims to solve the problem by adding to the count when special characters are detected
+ * 
+ * @param $str UTF-8 String
+ * @result integer
+ */
+module.exports = function ($str) {
+
+    if (typeof $str === 'undefined' || typeof $str.length === 'undefined') {
+
+        return 0;
+    }
+
+    var i = 0;
+    var c = $str.length;
+    if (typeof $str !== 'array') {
+
+        var tmp = [];
+        var isStr = typeof $str === 'string';
+        while (i < c) {
+
+            isStr ? tmp.push($str.charCodeAt(i))
+                  : tmp.push($str[i]);
+
+            i++;
+        }
+
+        $str = tmp;
+    }
+    else {
+
+        i = c;
+    }
+
+    while (i > 0) {
+
+        var code = $str[i];
+        if (code > 0x7f && code <= 0x7ff) {
+
+            c++;
+        }
+        else if (code > 0x7ff && code <= 0xffff) {
+
+            if (code < 0xDC00 || code > 0xDFFF) {
+
+                c += 2;
+            }
+        }
+
+        i--;
+    }
+
+    return c;
+};
